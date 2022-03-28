df380d8641e12a30f2031aac8a4fde72484e0eab
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-28 17:36:43 -0300

    last config

--- cookbooks/deploy_instances/recipes/deploy_frontend.rb
+++ cookbooks/deploy_instances/recipes/deploy_frontend.rb
@@ -13,6 +13,6 @@ bash 'deploy frontend' do
     code <<-EOH
     kubectl apply -f https://raw.githubusercontent.com/computerSmokio/rampupv2/main/kubernetes_related/frontend_deployment.yaml
     EOH
-    only_if 'kubectl get deployments frontend-movie-deployment -n rampup-frontend-ns > /dev/null 2>&1', user: 'ec2-user', environment: {'KUBECONFIG' => '/home/ec2-user/.kube/config'}
+    not_if 'kubectl get deployments frontend-movie-deployment -n rampup-frontend-ns > /dev/null 2>&1', user: 'ec2-user', environment: {'KUBECONFIG' => '/home/ec2-user/.kube/config'}
     action :run
 end
