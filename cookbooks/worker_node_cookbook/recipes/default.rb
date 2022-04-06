9e6fbb6c2e18017e5df6935317dbfd727a23b825
computerSmokio <mavargas@EN2110304.endava.net>
2022-04-06 08:42:46 -0300

    monitoring v0.5

--- cookbooks/worker_node_cookbook/recipes/default.rb
+++ cookbooks/worker_node_cookbook/recipes/default.rb
@@ -3,14 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
-
-bash 'change hostname' do
-    code <<-EOH
-    sudo hostnamectl set-hostname worker-cluster
-    EOH
-    action :run
-end
-
 directory '/etc/systemd/system/kubelet.service.d' do
     owner 'root'
     group 'root'
